fd7a784d9e2e9d4193559f2979ac7dde3b9da4a2
Zoe <ya_zhou@foxmail.com>
2020-10-26 17:20:06 +0800

    feat: 增加小贴士

--- resources/views/generator/tips.blade.php
+++ resources/views/generator/tips.blade.php
@@ -1,5 +1,6 @@
 <div>
     <pre class="layui-code">
 1. 弹出框想要滚动条的话，就对 layer.open 使用固定长度和宽度。
+2. 导出可以参考 protected\controllers\capacity\OrderDistributeController.php::actionListExport
     </pre>
 </div>
